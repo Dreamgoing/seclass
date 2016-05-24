19ea96134fc7469d13a033f9263c4910b22954c5
wangruoxuan <ruoxuanwanggoing@gmail.com>
2016-05-24 20:34:52 +0800

    first commit
    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+const double eps = 1e-6;
+int main() {
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
